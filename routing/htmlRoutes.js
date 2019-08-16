46e18938c3da2650bede5dcc5326f1341627d08c
Anitra <anitra.oursky@gmail.com>
2019-08-16 10:43:26 -0400

    add education and skills update html routes

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -18,5 +18,23 @@ module.exports = function(app) {
         }
     
       });
+      app.get("/education", function(err, res) {
+       
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("education");
+        }
+    
+      });
+      app.get("/skills", function(err, res) {
+       
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("skills");
+        }
+    
+      });
 
     };
